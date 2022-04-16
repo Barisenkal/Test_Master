0c5fdddc774287ca1582946929025d50da41dddc
Barisenkal <baris_senkal1997@hotmail.com>
2022-04-16 21:27:37 +0200

    160422

---
+++ app/src/main/java/com/example/test_master/Fahrzeugdaten.java
@@ -0,0 +1,14 @@
+package com.example.test_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fahrzeugdaten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fahrzeugdaten);
+    }
+}
---
+++ app/src/main/java/com/example/test_master/persDaten.java
@@ -0,0 +1,14 @@
+package com.example.test_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class persDaten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pers_daten);
+    }
+}
