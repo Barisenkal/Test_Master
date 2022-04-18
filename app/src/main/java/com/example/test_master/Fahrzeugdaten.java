ca245c732dc70085ae0fb4c84711600325aeb867
Barisenkal <baris_senkal1997@hotmail.com>
2022-04-18 18:11:29 +0200

    180422

--- app/src/main/java/com/example/test_master/Fahrzeugdaten.java
+++ app/src/main/java/com/example/test_master/Fahrzeugdaten.java
@@ -2,7 +2,10 @@ package com.example.test_master;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Fahrzeugdaten extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Fahrzeugdaten extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fahrzeugdaten);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Fahrzeugdaten.this,Termin_buchen.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/test_master/Termin_buchen.java
@@ -0,0 +1,14 @@
+package com.example.test_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Termin_buchen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termin_buchen);
+    }
+}
